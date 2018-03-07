aa5cabade3aee76c0b69cf7b632202a044f87562
StructureFold2 <32654227+StructureFold2@users.noreply.github.com>
2018-03-07 18:22:06 -0500

    Add files via upload

--- react_windows.py
+++ react_windows.py
@@ -8,7 +8,6 @@ used for any purpose. It may be a good idea to run this program several times, i
 and investigate all of these separately. 
 '''
 
-
 #Imports
 from itertools import islice
 from Bio import SeqIO
@@ -88,9 +87,11 @@ def hot_stepper(fasta_seqs,control_reacts,experimental_reacts,window=50,step=20)
             value_windows = [change[i:i+window] for i in xrange(0, len(change)-(window-1), step)]
             abs_value_windows = [abs_change[i:i+window] for i in xrange(0, len(change)-(window-1), step)]
             seq_windows = [v[i:i+window] for i in xrange(0, len(v)-(window-1), step)]
+            control_windows = [control[i:i+window] for i in xrange(0, len(v)-(window-1), step)]
+            exp_windows = [experimental[i:i+window] for i in xrange(0, len(v)-(window-1), step)]
             if len(value_windows) > 1:
                 for j in range(0, len(value_windows)):
-                    tidbit = tuple([get_sum(value_windows[j]),get_sum(abs_value_windows[j]) ,seq_windows[j],k,str(j)])
+                    tidbit = tuple([get_sum(value_windows[j]),get_sum(abs_value_windows[j]) ,seq_windows[j],k,str(j),control_windows[j],exp_windows[j]])
                     disaster_area.append(tidbit)
         except KeyError:
             continue
@@ -101,7 +102,7 @@ def ultra_dumper(catastrophe,out='bits.csv'):
     with open(out,'w') as g:
         g.write('net_change,total_change,seq,transcript,step\n')
         for item in catastrophe:
-            g.write(','.join([str(x) for x in item])+'\n')
+            g.write(','.join([str(x) for x in item[0:5]])+'\n')
 
 def write_out_fasta(info,outfyle='out.fasta',LW=80):
     '''Writes out the <.fasta> file, names are just transcript+step'''
@@ -113,28 +114,48 @@ def write_out_fasta(info,outfyle='out.fasta',LW=80):
             for i in xrange(0,len(seq),LW):
                 g.write(seq[i:i+LW] + '\n') 
 
+def check_extension(astring,extension):
+    '''Checks and fixes things to have the proper extension'''
+    out_string = astring if astring.endswith(extension) else astring + extension
+    return out_string
+
+def fork_react_dicts(giant_info_list):
+    '''Creates two sub-dictionaries in pseudo <.react> format'''
+    x_control,x_experimetal = {},{}
+    for item in giant_info_list:
+        name = '_'.join([item[3],item[4]])
+        control_frame, exp_frame = item[5],item[6]
+        x_control[name] = control_frame
+        x_experimetal[name] = exp_frame
+    return x_control,x_experimetal
+
+def write_react(react_info,outfyle='out.react'):
+    '''Writes the <.react> back out'''
+    with open(outfyle,'w') as g:
+        for transcript, data in react_info.items():
+            g.write(transcript+'\n')
+            g.write('\t'.join([str(q) for q in data])+'\n')
+
 #Main Function
 def main():
-    #print ''
-    #print '\033[1;4;94mStructureFold2:\033[0;0;92m react_windows.py\033[0m'
-    #print ''
     parser = argparse.ArgumentParser(description='Generate Windows of changing reactivity, will subtract control from experimental')
     parser.add_argument("control",type=str,help="control <.react> file")
     parser.add_argument("experimental",type=str,help="experimental <.react> file")
     parser.add_argument("fasta",type=str,help="<.fasta> to pull sequences from")
-    parser.add_argument('-wlen',type=int, default=50, help='<int> [default = 50] Window Length')
-    parser.add_argument('-wstep',type=int, default=20, help='<int> [default = 20] Window Step')
-    parser.add_argument('-outname',type=str,default='windows.csv', help='Change the name of the outfile, overrides default')
+    parser.add_argument('-wlen',type=int, default=50, help='[default = 50] Window Length')
+    parser.add_argument('-wstep',type=int, default=20, help='[default = 20] Window Step')
+    parser.add_argument('-outname',type=str,default=None, help='Change the name of the outfile, overrides default')
     parser.add_argument('-restrict',default = None, help = '<.txt > Limit analysis to these specific transcripts')
-    parser.add_argument('-filter_loss',action="store_true",default=False,help = 'Restrict output to largest reactivity losses')
-    parser.add_argument('-filter_gain',action="store_true",default=False,help = 'Restrict output to largest reactivity gains')
-    parser.add_argument('-filter_delta',action="store_true",default=False,help = 'Restrict output to most changed reactivity')
-    parser.add_argument('-perc',type=int, default=25, help='<int> [default = 25] Filter to this percent of windows')
+    parser.add_argument('-filter_loss',action="store_true",default=False,help = 'Filter output to largest reactivity losses')
+    parser.add_argument('-filter_gain',action="store_true",default=False,help = 'Filter output to largest reactivity gains')
+    parser.add_argument('-filter_delta',action="store_true",default=False,help = 'Filter output to most changed reactivity')
+    parser.add_argument('-perc',type=int, default=25, help='[default = 25] Filter to this percent of windows')
     parser.add_argument('-fastaout',action="store_true",default=False,help = 'Write windows to <.fasta> format as well')
+    parser.add_argument('-reactout',action="store_true",default=False,help = 'Write accompanying <.react> files as well')
     args = parser.parse_args()
     #
     default_name = '_'.join([args.control.replace('.react',''),args.experimental.replace('.react',''),str(args.wlen)+'win',str(args.wstep)+'step'])+'.csv'
-    out_name = default_name if args.outname == 'windows.csv' else args.outname
+    out_name = default_name if args.outname == None else check_extension(args.outname,'.csv')
     
     #Read in both groups of reactivities, fasta file with sequences
     control_reactivty,experimental_reactivty = read_reactivities(args.control),read_reactivities(args.experimental)
@@ -163,8 +184,27 @@ def main():
     
     #Output
     ultra_dumper(loud_noises,out_name)
+    
+    #Output Suite for <.fasta>
     if args.fastaout == True:
         write_out_fasta(loud_noises,out_name.replace('.csv','.fasta'))
+    
+    #Output Suite for <.react>
+    if args.reactout == True:
+        control_out,experimental_out = fork_react_dicts(loud_noises)
+        new_control_file = '_'.join([args.control.replace('.react',''),str(args.wlen)+'win',str(args.wstep)+'step'])+'.react'
+        new_exp_file = '_'.join([args.experimental.replace('.react',''),str(args.wlen)+'win',str(args.wstep)+'step'])+'.react'
+        if args.filter_loss == True:
+            new_control_file = new_control_file.replace('.react','_'+str(args.perc)+'loss.react')
+            new_exp_file = new_exp_file.replace('.react','_'+str(args.perc)+'loss.react')
+        if args.filter_gain == True:
+            new_control_file = new_control_file.replace('.react','_'+str(args.perc)+'gain.react')
+            new_exp_file = new_exp_file.replace('.react','_'+str(args.perc)+'gain.react')
+        if args.filter_delta == True:
+            new_control_file = new_control_file.replace('.react','_'+str(args.perc)+'delta.react')
+            new_exp_file = new_exp_file.replace('.react','_'+str(args.perc)+'delta.react')
+        write_react(control_out,new_control_file)
+        write_react(experimental_out,new_exp_file)
 
 if __name__ == '__main__': 
     main()
