e08f0b0d5295294435cc1bffdfbd0111d459c8b3
StructureFold2 <32654227+StructureFold2@users.noreply.github.com>
2019-12-04 18:18:25 -0500

    Add files via upload

--- sf2libs/structure_io.py
+++ sf2libs/structure_io.py
@@ -1,5 +1,24 @@
+import itertools
+from Bio import SeqIO
 
 def check_extension(astring,extension):
     '''Checks and fixes things to have the proper extension'''
     out_string = astring if astring.endswith(extension) else astring + extension
     return out_string 
+
+def read_fasta(afasta):
+    '''Fasta to Python dictionary'''
+    fasta_sequences = SeqIO.parse(open(afasta),'fasta')
+    return {record.id:str(record.seq) for record in fasta_sequences}
+
+def read_rtsc(rtsc_fyle):
+    '''Reads a <.rtsc> file into a dictionary, transcript_name:[list of stop numbers]'''
+    information = {}
+    with open(rtsc_fyle,'r') as f:
+        while True:
+            next_n_lines = list(itertools.islice(f, 3))
+            if not next_n_lines:
+                break
+            transcript,stops,empty_line = [n.strip() for n in next_n_lines]
+            information[transcript] = [int(x) for x in stops.split('\t')]
+    return information
