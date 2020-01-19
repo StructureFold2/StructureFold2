6a33679d392c10a103a27560b55f79693ffb7e98
StructureFold2 <32654227+StructureFold2@users.noreply.github.com>
2020-01-19 15:36:10 -0500

    Add files via upload

--- sf2libs/react_utils.py
+++ sf2libs/react_utils.py
@@ -1,6 +1,7 @@
 #Extra Tools for Reactivity Comparisons
 import re
 import itertools
+import collections
 
 class MotifEntry(object):
     '''Just holds all the components of an entry in a clean way'''
@@ -74,6 +75,50 @@ class MotifReport(object):
                 except KeyError:
                     continue
 
+class CompositionReport(object):
+    '''Generates and Contains a full Composition Report'''
+    def __init__(self,wsize,bases,percent,overlap,fasta_dict,react_A,react_B,FP,TP,fill='-'):
+        self.wsize = wsize
+        self.bases = bases
+        self.percent = percent
+        self.overlap = overlap
+        self.FP = FP
+        self.TP = TP
+        self.size = FP+wsize+TP
+        self.records = {}
+        self.query = '_'.join([str(x) for x in [wsize,bases,percent]])
+        for transcript,sequence in fasta_dict.items():
+                try:
+                    A,B = react_A[transcript],react_B[transcript]
+                    delta = subtract_reacts(B,A)
+                    matches = composition_coords(sequence,bases,percent,wsize,overlap)
+                    vectors = [A,B,delta,sequence]
+                    for m in matches:
+                        coreA,coreB,coreD,coreS = [core[m[0]:m[1]] for core in vectors]
+                        fpA,fpB,fpD,fpS = [fp[max(m[0]-FP,0):m[0]] for fp in vectors]
+                        tpA,tpB,tpD,tpS = [tp[m[1]:min(len(tp)-1,m[1]+TP)] for tp in vectors]
+                        
+                        if len(fpS) < FP:
+                            fpfill = (FP-len(fpS))*fill
+                            fpS = fpfill+fpS
+                            fpA,fpB,fpD = [list(fpfill)+orange for orange in [fpA,fpB,fpD]]
+
+                        if len(tpS) < TP:
+                            tpfill = (TP-len(tpS))*fill
+                            tpS = tpS+tpfill
+                            tpA,tpB,tpD = [lemon + list(tpfill) for lemon in [tpA,tpB,tpD]]
+                        
+                        q_items = {'A':coreA,'B':coreB,'delta':coreD,'seq':coreS}
+                        fp_items = {'fp_A':fpA,'fp_B':fpB,'fp_delta':fpD,'fp_seq':fpS}
+                        tp_items = {'tp_A':tpA,'tp_B':tpB,'tp_delta':tpD,'tp_seq':tpS}
+                        q_items.update(delta_metrics(coreD,'query'))
+                        fp_items.update(delta_metrics(fpD,'fp'))
+                        tp_items.update(delta_metrics(tpD,'tp'))
+                        entry = MotifEntry(transcript,self.query,m,q_items,fp_items,tp_items)
+                        self.records[entry.generate_key()] = entry
+                except KeyError:
+                    continue
+
 def permute_motif(motif):
     '''Returns a list of all possible permutations of a DNA motif'''
     if all([base in dna for base in motif]):
@@ -113,6 +158,31 @@ def delta_metrics(delta_list,prefix):
     labels = ['change','increases','decreases','abs_change']
     prefixed_lables = ['_'.join([prefix,lab]) for lab in labels]
     return {numb:label for numb,label in zip(prefixed_lables,sums)}
+
+def check_window_comp(seq,bases,percent):
+    '''Checks a sequence for passing composition criteria'''
+    comp = collections.Counter(seq)
+    sum_hits = sum([comp[x] for x in bases])
+    if float(sum_hits)/sum(comp.values()) >= percent:
+        return True
+    else:
+        return False
+
+def composition_coords(seq,bases,percent,wsize=8,overlap=True):
+    '''dimentional compute'''
+    windows = [(seq[i:i+wsize],i,i+wsize) for i in range(0,len(seq)-wsize+1)]
+    windows = filter(lambda i: check_window_comp(i[0],bases,percent),windows)
+    windows = [item[1:] for item in windows]
+    if overlap:
+        return windows
+    else:
+        filtered = []
+        for win in windows:
+            if not filtered:
+                filtered.append(win)
+            elif win[0] > filtered[-1][1]:
+                filtered.append(win)
+        return filtered
 
 #The standard DNA wildcard alphabet
 wildcards = {'N':['A','C','G','T'],
